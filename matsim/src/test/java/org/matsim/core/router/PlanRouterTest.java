7391aaa291c8d5e0f0e0a5a0f3a707547b01fdbd
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-03-14 03:47:13 +0100

    Add test for summing up travel times and considering activity end times

--- matsim/src/test/java/org/matsim/core/router/PlanRouterTest.java
+++ matsim/src/test/java/org/matsim/core/router/PlanRouterTest.java
@@ -21,18 +21,18 @@ import org.matsim.facilities.Facility;
 public class PlanRouterTest {
 	@Test
 	public void testDepartureTime() {
-		run(ActivityDurationInterpretation.tryEndTimeThenDuration, false);
-		run(ActivityDurationInterpretation.tryEndTimeThenDuration, true);
-		run(ActivityDurationInterpretation.minOfDurationAndEndTime, false);
-		run(ActivityDurationInterpretation.minOfDurationAndEndTime, true);
+		runDepartureTime(ActivityDurationInterpretation.tryEndTimeThenDuration, false);
+		runDepartureTime(ActivityDurationInterpretation.tryEndTimeThenDuration, true);
+		runDepartureTime(ActivityDurationInterpretation.minOfDurationAndEndTime, false);
+		runDepartureTime(ActivityDurationInterpretation.minOfDurationAndEndTime, true);
 	}
 
-	private void run(ActivityDurationInterpretation interpretation, boolean useDuration) {
+	private void runDepartureTime(ActivityDurationInterpretation interpretation, boolean useDuration) {
 		Config config = ConfigUtils.createConfig();
 		config.plans().setActivityDurationInterpretation(interpretation);
 
 		TripRouter.Builder builder = new TripRouter.Builder(config);
-		builder.setRoutingModule("generic", new GenericRoutingModule());
+		builder.setRoutingModule("generic", new GenericRoutingModule(50.0));
 		TripRouter tripRouter = builder.build();
 
 		PlanRouter router = new PlanRouter(tripRouter);
@@ -81,13 +81,113 @@ public class PlanRouterTest {
 		}		
 	}
 
+	@Test
+	public void testSummingUpTravelTimesAndActivityDurations() {
+		Config config = ConfigUtils.createConfig();
+		config.plans().setActivityDurationInterpretation(ActivityDurationInterpretation.tryEndTimeThenDuration);
+
+		TripRouter.Builder builder = new TripRouter.Builder(config);
+		builder.setRoutingModule("generic", new GenericRoutingModule(100.0));
+		TripRouter tripRouter = builder.build();
+
+		PlanRouter router = new PlanRouter(tripRouter);
+
+		Plan plan = PopulationUtils.createPlan();
+
+		Activity activity;
+		Leg leg;
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setEndTime(1000.0);
+		plan.addActivity(activity);
+
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setMaximumDuration(1000.0);
+		plan.addActivity(activity);
+		
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setMaximumDuration(1000.0);
+		plan.addActivity(activity);
+		
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		plan.addActivity(activity);
+
+		router.run(plan);
+
+		assertEquals(1000.0, ((Leg) plan.getPlanElements().get(1)).getDepartureTime(), 1e-3);
+		assertEquals(2100.0, ((Leg) plan.getPlanElements().get(3)).getDepartureTime(), 1e-3);
+		assertEquals(3200.0, ((Leg) plan.getPlanElements().get(5)).getDepartureTime(), 1e-3);
+	}
+	
+	@Test
+	public void testSummingUpTravelTimesAndActivityEndTimes() {
+		Config config = ConfigUtils.createConfig();
+		config.plans().setActivityDurationInterpretation(ActivityDurationInterpretation.tryEndTimeThenDuration);
+
+		TripRouter.Builder builder = new TripRouter.Builder(config);
+		builder.setRoutingModule("generic", new GenericRoutingModule(1000.0));
+		TripRouter tripRouter = builder.build();
+
+		PlanRouter router = new PlanRouter(tripRouter);
+
+		Plan plan = PopulationUtils.createPlan();
+
+		Activity activity;
+		Leg leg;
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setEndTime(1000.0);
+		plan.addActivity(activity);
+
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setEndTime(1200.0); // This is before arrival! Actual arrival is at 2000.0
+		plan.addActivity(activity);
+		
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		activity.setEndTime(1400.0); // This is before arrival! Actual arrival is at 3000.0
+		plan.addActivity(activity);
+		
+		leg = PopulationUtils.createLeg("generic");
+		plan.addLeg(leg);
+
+		activity = PopulationUtils.createActivityFromCoord("generic", new Coord(0.0, 0.0));
+		plan.addActivity(activity);
+
+		router.run(plan);
+
+		assertEquals(1000.0, ((Leg) plan.getPlanElements().get(1)).getDepartureTime(), 1e-3);
+		assertEquals(2000.0, ((Leg) plan.getPlanElements().get(3)).getDepartureTime(), 1e-3);
+		assertEquals(3000.0, ((Leg) plan.getPlanElements().get(5)).getDepartureTime(), 1e-3);
+	}
+	
 	static private class GenericRoutingModule implements RoutingModule {
+		private final double travelTime;
+		
+		GenericRoutingModule(double travelTime) {
+			this.travelTime = travelTime;
+		}
+		
 		@Override
 		public List<? extends PlanElement> calcRoute(Facility fromFacility, Facility toFacility, double departureTime,
 				Person person) {
 			Leg leg = PopulationUtils.createLeg("generic");
 			leg.setDepartureTime(departureTime);
-			leg.setTravelTime(50.0);
+			leg.setTravelTime(travelTime);
 			return Collections.singletonList(leg);
 		}
 	}
